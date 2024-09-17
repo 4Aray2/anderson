9c6d97f9e12e49e0bcb6d6aebe6d63602131a6fd
baimo <baimolda.2001@gmail.com>
2024-09-18 01:13:11 +0500

    hw

---
+++ src/main/java/CollectionService.java
@@ -0,0 +1,64 @@
+import collections.ArrayList;
+import collections.HashSet;
+
+public class CollectionService {
+    public static void main(String[] args) {
+        arrayTest();
+        setTest();
+    }
+
+    private static void arrayTest() {
+        ArrayList arrayList = new ArrayList(5);
+        arrayList.put(1);
+        arrayList.put(2);
+        arrayList.put(3);
+        arrayList.put(4);
+        arrayList.put(5);
+
+        System.out.println(arrayList);
+
+        // doubles size after this put
+        arrayList.put(6);
+        System.out.println(arrayList);
+
+
+        arrayList.delete(1);
+        arrayList.delete(1);
+
+        // halves size after this delete
+        arrayList.delete(1);
+        System.out.println(arrayList);
+
+        System.out.println(arrayList.get(0));
+//        System.out.println(arrayList.get(-1)); // ArrayIndexOutOfBoundsException
+//        System.out.println(arrayList.get(3)); // ArrayIndexOutOfBoundsException
+    }
+
+    private static void setTest() {
+        HashSet set = new HashSet();
+        set.put(1);
+        set.put(1);
+        set.put(2);
+        set.put(2);
+        set.put(3);
+        set.put(4);
+        set.put(5);
+        set.put(6);
+        set.put(6);
+        for (Integer integer : set) {
+            System.out.print(integer + " ");
+        }
+        System.out.println();
+
+        System.out.println(set.contains(6));
+        System.out.println(set.contains(7));
+
+        set.delete(6);
+        System.out.println(set.contains(6));
+
+
+        for (Integer integer : set) {
+            System.out.print(integer + " ");
+        }
+    }
+}
---
+++ src/main/java/collections/HashSet.java
@@ -0,0 +1,103 @@
+package collections;
+
+import java.util.Iterator;
+
+public class HashSet implements Iterable<Integer> {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    private ListNode[] set;
+    private int buckets;
+
+    public HashSet() {
+        buckets = 10;
+        set = new ListNode[buckets];
+        for (int i = 0; i < buckets; i++) {
+            set[i] = new ListNode(0);
+        }
+    }
+
+    public void put(int key) {
+        ListNode current = set[getHash(key)];
+        while (current.next != null) {
+            if (current.next.val == key) return;
+            current = current.next;
+        }
+        current.next = new ListNode(key);
+    }
+
+    public void delete(int key) {
+        ListNode current = set[getHash(key)];
+        while (current.next != null) {
+            if (current.next.val == key) {
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+    }
+
+    public boolean contains(int key) {
+        ListNode current = set[getHash(key)];
+        while (current.next != null) {
+            if (current.next.val == key) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    private int getHash(int key) {
+        return key % buckets;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            private int bucketIndex = 0;
+            private ListNode currentNode = set[bucketIndex];
+
+            @Override
+            public boolean hasNext() {
+                if (currentNode != null && currentNode.next != null) {
+                    return true;
+                }
+
+                for (int i = bucketIndex + 1; i < buckets; i++) {
+                    if (set[i].next != null) {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+            @Override
+            public Integer next() {
+                if (currentNode != null && currentNode.next != null) {
+                    currentNode = currentNode.next;
+                    return currentNode.val;
+                }
+
+                bucketIndex++;
+                while (bucketIndex < buckets && set[bucketIndex].next == null) {
+                    bucketIndex++;
+                }
+
+                if (bucketIndex >= buckets) {
+                    throw new java.util.NoSuchElementException();
+                }
+
+                currentNode = set[bucketIndex].next;
+                return currentNode.val;
+            }
+        };
+    }
+}
